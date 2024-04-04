448f7b425c614795d5672d35adcd6b695a5421c4
blacksmith <101953303+bblacksmithh@users.noreply.github.com>
2024-04-04 11:31:24 +0200

    Sidebar active menu Items fixed

--- Front End/src/Components/SideMenu/SideMenu.tsx
+++ Front End/src/Components/SideMenu/SideMenu.tsx
@@ -6,15 +6,20 @@ import Link from "next/link";
 const SideMenu = ({current}:{current:string}) => {
     const [currentTab, setCurrentTab] = useState(current);
     const {styles, cx} = useStyles();
+
+    const handleClick = (e:any) => {
+        console.log('click ', e);
+        setCurrentTab(e.key);
+    }
     return (
         <main className={styles.main}>
             <Menu
-            selectedKeys={[currentTab]} className={styles.menuStyle} mode="vertical">
-                <Menu.Item className={styles.menuItem}><Link href='/transactions'>Transactions</Link></Menu.Item>
-                <Menu.Item className={styles.menuItem}><Link href='books'>Books</Link></Menu.Item>
-                <Menu.Item className={styles.menuItem}><Link href='/genres'>Genres</Link></Menu.Item>
-                <Menu.Item className={styles.menuItem}><Link href='/members'>Members</Link></Menu.Item>
-                <Menu.Item className={styles.menuItem}><Link href='/librarians'>Librarians</Link></Menu.Item>
+            selectedKeys={[currentTab]} className={styles.menuStyle} mode="vertical" >
+                <Menu.Item onClick={handleClick} key='transactions' className={styles.menuItem}><Link href='/transactions'>Transactions</Link></Menu.Item>
+                <Menu.Item onClick={handleClick} key='books' className={styles.menuItem}><Link href='books'>Books</Link></Menu.Item>
+                <Menu.Item onClick={handleClick} key='genres' className={styles.menuItem}><Link href='/genres'>Genres</Link></Menu.Item>
+                <Menu.Item onClick={handleClick} key='members' className={styles.menuItem}><Link href='/members'>Members</Link></Menu.Item>
+                <Menu.Item onClick={handleClick} key='librarians' className={styles.menuItem}><Link href='/librarians'>Librarians</Link></Menu.Item>
             </Menu>
         </main>
     );
